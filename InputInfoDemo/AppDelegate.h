b6d317e36761d4c2c4e7b2f7179390d377b067c8
czz845752072 <chuzhaozhi@126.com>
2017-05-27 10:44:26 +0800

    Initial Commit

---
+++ InputInfoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InputInfoDemo
+//
+//  Created by chuzhaozhi on 2017/5/27.
+//  Copyright © 2017年 chuzhaozhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InputInfoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InputInfoDemo
+//
+//  Created by chuzhaozhi on 2017/5/27.
+//  Copyright © 2017年 chuzhaozhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
